211490c38cefb54a2a7bf5e61586e0ef970d1c38
Azzo <49626045+AzizBenAmmar@users.noreply.github.com>
2021-02-07 16:27:23 +0100

    Update Attention_fo_Neural_Machine_Translation.py

--- NLP/NLP and RNNs/Attention_fo_Neural_Machine_Translation.py
+++ NLP/NLP and RNNs/Attention_fo_Neural_Machine_Translation.py
@@ -1,7 +1,3 @@
-from __future__ import print_function, division
-from builtins import range, input
-# Note: you may need to update your version of future
-# sudo pip install -U future
 
 import os, sys
 
